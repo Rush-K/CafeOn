2ea05dcc28369989f59d7a724aa662b3689de734
kms <k_rush@naver.com>
2021-07-04 21:44:58 +0900

    개인 프로젝트 시작

---
+++ app/src/main/java/com/example/cafeon/loginActivity.kt
@@ -0,0 +1,15 @@
+package com.example.cafeon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class loginActivity : AppCompatActivity() {
+    val builder = AlertDialog.Builder(this)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    
+}
