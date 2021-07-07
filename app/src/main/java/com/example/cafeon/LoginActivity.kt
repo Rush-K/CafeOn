c7ca775e48b9c89a4d84c97fe14b39ddeb2db5a2
kms <k_rush@naver.com>
2021-07-07 20:51:32 +0900

    신규유저 닉네임 입력 구현

--- app/src/main/java/com/example/cafeon/LoginActivity.kt
+++ app/src/main/java/com/example/cafeon/LoginActivity.kt
@@ -7,14 +7,19 @@ import android.view.View
 import android.widget.Toast
 import com.example.cafeon.databinding.ActivityLoginBinding
 import com.example.cafeon.databinding.ActivityMainBinding
+import com.example.cafeon.datamodel.UserInfo
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
+
 //import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() { // ? : null 일 수 있음, !! : null 이 아님
 
     // Firebase Authentication 관리 클래스
     var auth: FirebaseAuth? = null
+    var firestore: FirebaseFirestore? = null
+    var userInformation: UserInfo? = null // 유저 정보가 담기는 데이터 클래스
     private lateinit var loginActivityBinding: ActivityLoginBinding // 액티비티 바인딩
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +29,7 @@ class LoginActivity : AppCompatActivity() { // ? : null 일 수 있음, !! : nul
         setContentView(loginActivityBinding.root)
 
         auth = FirebaseAuth.getInstance() // Firebase 객체 생성
+        firestore = FirebaseFirestore.getInstance() // Firestore 객체 생성
         loginActivityBinding.emailLoginButton.setOnClickListener { emailLogin() }
     }
 
@@ -44,7 +50,7 @@ class LoginActivity : AppCompatActivity() { // ? : null 일 수 있음, !! : nul
 
                 if (task.isSuccessful) {
                     Toast.makeText(this, getString(R.string.signup_complete), Toast.LENGTH_SHORT).show()
-                    moveMainPage(auth?.currentUser)
+                    movePage(auth?.currentUser)
                 } else if (task.exception?.message.isNullOrEmpty()) { // 회원가입 에러
                     Toast.makeText(this, task.exception!!.message, Toast.LENGTH_SHORT).show()
                 } else { // 이미 계정이 있는 경우
@@ -60,18 +66,35 @@ class LoginActivity : AppCompatActivity() { // ? : null 일 수 있음, !! : nul
                 loginActivityBinding.progressBar.visibility = View.GONE
 
                 if (task.isSuccessful) { // 로그인 성공
-                    moveMainPage(auth?.currentUser)
+                    movePage(auth?.currentUser)
                 } else { // 로그인 실패
                     Toast.makeText(this, task.exception!!.message, Toast.LENGTH_SHORT).show()
                 }
             }
     }
 
-    fun moveMainPage(user : FirebaseUser?) {
+    fun movePage(user : FirebaseUser?) {
         if (user != null) {
-            Toast.makeText(this, getString(R.string.signin_complete), Toast.LENGTH_SHORT).show()
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
+            firestore?.collection("UserInfo")?.document(user.uid)?.get()?.addOnCompleteListener {
+                    task ->
+                if (task.isSuccessful) {
+                    userInformation = task.result?.toObject(UserInfo::class.java)
+                    if (userInformation != null) { // 유저정보가 있는 경우
+                        var intent = Intent(this, MainActivity::class.java)
+                        intent.putExtra("userinformation", userInformation)
+                        startActivity(intent)
+                        finish()
+                    } else { // 유저정보가 없는 경우
+                        var intent = Intent(this, NewUserActivity::class.java)
+                        intent.putExtra("email", user.email)
+                        intent.putExtra("uid", user.uid)
+                        startActivity(intent)
+                        finish()
+                    }
+                } else { // Firestore 접속 실패
+
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/cafeon/MainActivity.kt
+++ app/src/main/java/com/example/cafeon/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.cafeon
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.example.cafeon.databinding.ActivityLoginBinding
+import com.example.cafeon.databinding.ActivityMainBinding
+import com.example.cafeon.datamodel.UserInfo
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainActivityBinding: ActivityMainBinding // 액티비티 바인딩
+    var firestore: FirebaseFirestore? = null
+    var userInformation: UserInfo? = null // 유저 정보가 담기는 데이터 클래스
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainActivityBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(mainActivityBinding.root)
+
+        firestore = FirebaseFirestore.getInstance();
+        runBlocking {
+            userInformation = intent.getSerializableExtra("userinformation") as UserInfo
+        }
+
+        mainActivityBinding.userId.text = userInformation!!.nickname.toString().plus("님, 안녕하세요 :)")
     }
 }
